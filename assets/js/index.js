97d8bf028b1b85b24e41884487b4b295b7779918
Lily-96 <LILIYASAVITSKA@GMAIL.COM>
2025-05-07 12:12:07 +0200

    provaForeach

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,28 +4,30 @@ const arrayIdPlaylist = [125, 118, 27, 55, 123, 13, 77, 86];
 const rowPlaylist = document.querySelector(".rowPlaylist ");
 
 const searchPlaylist = () => {
-  fetch(`${UrlPlaylist} ${query}`, {
-    headers: {
-      "x-rapidapi-key": token,
-      "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
-    },
-  })
-    .then((resp) => {
-      console.log(resp);
-      if (!resp.ok) {
-        if (resp.status >= 500) {
-          throw new Error("Errore lato server");
-        } else {
-          throw new Error("Errore nella fetch");
-        }
-      }
-      return resp.json();
+  arrayIdPlaylist.forEach((id) => {
+    query = id;
+    fetch(`${UrlPlaylist} ${query}`, {
+      headers: {
+        "x-rapidapi-key": token,
+        "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
+      },
     })
-    .then((playlist) => {
-      //   playlist.forEach((onePlaylist) => {
-      const col = document.createElement("div");
-      col.className = "col-3";
-      col.innerHTML = `<div class="rapidAccess d-flex align-items-center">
+      .then((resp) => {
+        console.log(resp);
+        if (!resp.ok) {
+          if (resp.status >= 500) {
+            throw new Error("Errore lato server");
+          } else {
+            throw new Error("Errore nella fetch");
+          }
+        }
+        return resp.json();
+      })
+      .then((playlist) => {
+        //   playlist.forEach((onePlaylist) => {
+        const col = document.createElement("div");
+        col.className = "col-3";
+        col.innerHTML = `<div class="rapidAccess d-flex align-items-center">
                   <div class="me-3 flex-shrink-0">
                     <img class="img-fluid rounded-3" src= ${playlist.picture_small} style="width: 80px; height: 80px; object-fit: cover" />
                   </div>
@@ -33,14 +35,15 @@ const searchPlaylist = () => {
                     <p class="fw-bold mb-1 fs-6">${playlist.title}</p>
                   </div>
 `;
-      rowPlaylist.appendChild(col);
-      //   });
-    })
+        rowPlaylist.appendChild(col);
+        //   });
+      })
 
-    .catch((error) => {
-      console.log(error);
-      alert(error.message);
-    });
+      .catch((error) => {
+        console.log(error);
+        alert(error.message);
+      });
+  });
 };
 
 window.onload = function () {
